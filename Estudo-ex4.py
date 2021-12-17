b7a381f916407e649177dc6d5deb14b6fbf1134d
vinigpdias <viniciusgustavo.p.dias@gmail.com>
2021-12-17 15:00:09 -0300

    estudo

---
+++ Estudo-ex4.py
@@ -0,0 +1,9 @@
+#Estudo ex:4
+a = int(input("Digite o valor de a: "))
+b = int(input("Digite o valor de b: "))
+
+if a % 2 == 0 or b % 2 == 0:
+    print("Ao menos um Par")
+
+else:
+    print("Nenhum dos dois é Par")
---
+++ Estudo-ex5.py
@@ -0,0 +1,15 @@
+a = int(input("Digite valor de a: "))
+b = int(input("Digite valor de b: "))
+c = 0
+
+if a % 2 == 0:
+    print("O primeiro é Par")
+    c = 1
+
+if b % 2 == 0: 
+    print("O segundo é Par")
+    c = 1
+    
+if c == 0:
+    print("Nenhum dos dois é Par")
+    
