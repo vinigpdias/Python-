dcb5b4eebbb074b01acfebf770c1f7f709374cb7
vinigpdias <viniciusgustavo.p.dias@gmail.com>
2021-12-21 16:29:12 -0300

    Estudo

---
+++ Estudo-ex8.py
@@ -0,0 +1,13 @@
+a = int(input("Digite o valor de a: "))
+b = int(input("Digite o valor de b: "))
+c = int(input("Digite o valor de c: "))
+
+if a > b and a > c:
+    print(a)
+
+if b > a and b > c:
+    print(b)
+
+if c > a and c > b:
+    print(c)
+    
